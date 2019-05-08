59f046edcd8120b11e06cf17a53819cbf9ec0616
artwin22 <muhamad.14116050@student.itera.ac.id>
2019-05-08 22:15:10 +0700

    Splash Screen
    Tampilan Splash Screen

---
+++ app/src/main/java/com/itera/iteratime/Splashscreen.kt
@@ -0,0 +1,24 @@
+package com.itera.iteratime
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class Splashscreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splashscreen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },4000)
+    }
+}
