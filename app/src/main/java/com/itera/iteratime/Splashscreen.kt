e8dc792f43070c39b8e2c3d9e20c056e49cedb0d
artwin22 <muhamad.14116050@student.itera.ac.id>
2019-05-08 23:30:48 +0700

    Change Icon APK & Fix Splash Screen

--- app/src/main/java/com/itera/iteratime/Splashscreen.kt
+++ app/src/main/java/com/itera/iteratime/Splashscreen.kt
@@ -11,7 +11,6 @@ class Splashscreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splashscreen)
         window.requestFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splashscreen)
