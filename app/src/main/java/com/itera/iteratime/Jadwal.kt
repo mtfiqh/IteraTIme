04fef9687d853d4240a2d316f60bd16cb048a5e4
taufiq2810 <taufiq28101998@gmail.com>
2019-05-12 13:07:34 +0700

    add read data from firebase untuk senin

--- app/src/main/java/com/itera/iteratime/Jadwal.kt
+++ app/src/main/java/com/itera/iteratime/Jadwal.kt
@@ -1,12 +1,21 @@
 package com.itera.iteratime
 
 class Jadwal(
-    var mataKuliah: String?="",
-    var sks: Int?=0,
-    var hari: String?="",
-    var dari:String?="",
-    var sampai:String?="",
-    var gedung:String?="",
-    var ruangan:String?="",
-    var dosen:String?=""
-)
+    var mataKuliah: String,
+    var sks: Int,
+    var hari: String,
+    var dari:String,
+    var sampai:String,
+    var gedung:String,
+    var ruangan:String,
+    var dosen:String
+){
+    constructor() : this("", 0, "", "", "", "", "", "")
+
+    override fun toString(): String {
+        return "Mata Kuliah: "+mataKuliah+"\n" +
+                "sks: "+sks+"\n" +
+                "Hari: "+hari+"\n"+"" +
+                "dari: "+dari
+    }
+}
--- app/src/main/java/com/itera/iteratime/ui/main/SeninFragment.kt
+++ app/src/main/java/com/itera/iteratime/ui/main/SeninFragment.kt
@@ -8,14 +8,17 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import android.widget.LinearLayout
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.itera.iteratime.Jadwal
 import com.itera.iteratime.JadwalAdapterR
 
 import com.itera.iteratime.R
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -31,18 +34,53 @@ class SeninFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         val v= inflater.inflate(R.layout.fragment_senin, container, false)
         val rView = v.findViewById<RecyclerView>(R.id.recycleView)
 
         rView.layoutManager = LinearLayoutManager(v.context, LinearLayout.VERTICAL, false)
-        val jadwalList = ArrayList<Jadwal>()
+        var jadwalList = ArrayList<Jadwal>()
+        var adapter =JadwalAdapterR(jadwalList)
 
-        
-        jadwalList.add(Jadwal("x", 2, "Senin", "10:00", "12:00", "C", "204", "iqbal" ))
-        val adapter=JadwalAdapterR(jadwalList)
+        val database = FirebaseDatabase.getInstance();
+        val myRef = database.getReference().ref.child("Jadwal").child("Senin")
 
-        rView.adapter=adapter
+        val jadwalListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                print(dataSnapshot)
+                if(dataSnapshot!!.exists()){
+                    for(j in dataSnapshot.children){
+                        val jadwal = j.getValue(Jadwal::class.java)
+//                        println(j.key)
+//                        println(jadwal.toString())
+//                        jadwalList.add(j.getValue(Jadwal::class.java)!!)
+                            println("check")
+                            jadwalList.add(jadwal!!)
+                            println("size: "+jadwalList.size)
+                            println("===============================")
+                            rView.adapter=adapter
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+//                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+                // ...
+            }
+        }
+
+        myRef.addValueEventListener(jadwalListener)
+
+
+        adapter =JadwalAdapterR(jadwalList)
+        println("size")
+        println(jadwalList.size)
+        println("=========================")
+
+
         return v
     }
 
